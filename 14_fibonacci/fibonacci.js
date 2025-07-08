e6607037fd8eec2a729748c30b18df4d50fbe5fa
nxdal02 <nedalt48@gmail.com>
2025-07-08 15:31:58 +0100

    completed 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if  (n < 0) {
+        return "OOPS";
+    }
+    let prev = 0;
+    let cur = 1;
+    for (let i = 2; i <= n; i++) {
+        [cur, prev] = [cur + prev, cur];
+    }
+    return cur;
+
+
+    
 
 };
 
