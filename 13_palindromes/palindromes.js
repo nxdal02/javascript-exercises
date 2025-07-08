e3e79fe08207b1c71475615242848490f5da7f0c
nxdal02 <nedalt48@gmail.com>
2025-07-08 12:39:23 +0100

    completed 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (s) {
+    let str = s.toLowerCase().split('').filter((letter) => letter.match(/[a-z0-9]/i));
+    let j = str.length - 1;
+    for(let i = 0; i < j; i++){
+        if (str[i] != str[j]){
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
